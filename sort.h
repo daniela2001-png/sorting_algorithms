c7f607463a2274381d6cea0167acaa07f405ce47
daniela2001-png <dmbmora@outlook.com>
2020-10-19 17:28:43 -0500

    añado las nuevas funciones a mi header

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t **list, listint_t *derecha, listint_t *izquierda);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
